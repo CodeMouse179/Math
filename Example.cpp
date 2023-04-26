41dd5147d650e198f9b06d59318f7f45b031a5aa
CodeMouse179 <codemouse179@gmail.com>
2023-04-26 16:52:53 +0300

    add *

---
+++ Example.cpp
@@ -0,0 +1,6 @@
+#include "Math.hpp"
+
+int main()
+{
+    return 0;
+}
---
+++ Math.hpp
@@ -0,0 +1,12 @@
+#ifndef SYSTEM_MATH_HPP
+#define SYSTEM_MATH_HPP
+
+namespace System
+{
+    class Math
+    {
+    public:
+    };
+}
+
+#endif
