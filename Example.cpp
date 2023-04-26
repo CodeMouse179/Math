7b70a88c0a0aafc3adb476e68dc5bcd600d7055d
CodeMouse179 <codemouse179@gmail.com>
2023-04-26 17:17:15 +0300

    add E, PI, Tau, set c++ 11

--- Example.cpp
+++ Example.cpp
@@ -1,6 +1,11 @@
 #include "Math.hpp"
+#include <iostream>
+#include <iomanip>
 
 int main()
 {
+    std::cout << "e   : " << std::setprecision(17) << System::Math::E << "\n";
+    std::cout << "pi  : " << std::setprecision(17) << System::Math::PI << "\n";
+    std::cout << "tau : " << std::setprecision(17) << System::Math::Tau << "\n";
     return 0;
 }
--- Math.hpp
+++ Math.hpp
@@ -6,6 +6,11 @@ namespace System
     class Math
     {
     public:
+        static constexpr double E = 2.7182818284590451;
+
+        static constexpr double PI = 3.1415926535897931;
+
+        static constexpr double Tau = 6.2831853071795862;
     };
 }
 
